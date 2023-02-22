d97e0142a7b4eaa0134172a3e7911afea32ec1c3
Andrew <andrew.wildwesttech@gmail.com>
2023-02-22 01:10:29 -0500

    eod

--- modules/emr-studio/bootstrap-scripts/bootstrap.sh
+++ modules/emr-studio/bootstrap-scripts/bootstrap.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Install packages
-pip install boto3 awswrangler pandas
+# Install packages for PySpark
+sudo /usr/bin/python3 -m pip install boto3 pandas numpy==1.17.3 scikit-learn
 
-# Download and install jars from S3
-#aws s3 cp s3://my-bucket/my-jar-file-1.jar /usr/lib/my-jars/
-#aws s3 cp s3://my-bucket/my-jar-file-2.jar /usr/lib/my-jars/
+# Install packages for regular Python
+sudo /usr/bin/python3 -m ensurepip
+sudo /usr/bin/python3 -m pip install boto3 pandas numpy==1.17.3 scikit-learn
+
+# Fix awswrangler compatibility issue with numpy
+sudo /usr/bin/python3 -m pip install sudo pip install pyarrow==2 awswrangler
+
+# Download JAR files from S3
+aws s3 cp s3://wildwesttech-emr-studio-resources-dev/jar-files/mssql-jdbc-6.4.0.jre8.jar /usr/lib/spark/jars/mssql-jdbc-6.4.0.jre8.jar
+aws s3 cp s3://wildwesttech-emr-studio-resources-dev/jar-files/postgresql-42.5.4.jar /usr/lib/spark/jars/postgresql-42.5.4.jar
